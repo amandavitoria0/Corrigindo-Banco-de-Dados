266d50b8549fa6b9a7c8494e3a1899492fa835ae
amanda <amanda.v.barros6@aluno.senai.br>
2025-08-14 10:37:00 -0500

    corrigindo

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -14,7 +14,7 @@ CREATE TABLE Alunos (
 
 INSERT INTO alunos (id, nome, data_nascimento, situacao, telefone, endereco) VALUES
 ('100041138', 'Aline Sousa Bezerra', '2007-12-16', 'Matriculado', '(68) 99901-0001', 'Rua das Flores, Centro, Rio Branco - AC'),
-('200041139', 'Amanda VitÃ³ria de Barros', '2003-12-05', 'Matriculado', '(68) 99901-0002', 'Rua das Flores, Centro, Rio Branco - AC'),
+('200041139', 'Amanda Vitória de Barros', '2003-12-05', 'Matriculado', '(68) 99901-0002', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('300041117', 'Ana Sara Santos do Nascimento', '2009-11-27', 'Matriculado', '(68) 99901-0003', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('400041152', 'Brenda Camily Lima da Silva', '2007-12-02', 'Matriculado', '(68) 99901-0004', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('500041149', 'Calebe Barbosa de Andrade Correia', '2010-05-09', 'Matriculado', '(68) 99901-0005', 'Rua das Flores, Centro, Rio Branco - AC'),
@@ -24,7 +24,7 @@ INSERT INTO alunos (id, nome, data_nascimento, situacao, telefone, endereco) VAL
 ('900032830', 'Giovana Monteiro de Souza', '2006-12-07', 'Matriculado', '(68) 99901-0009', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('1100041120', 'Gustavo do Carmo Jesus', '2004-12-31', 'Matriculado', '(68) 99901-0010', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('1200041131', 'Izabele Lorrany Barreto Matias', '2004-04-20', 'Matriculado', '(68) 99901-0011', 'Rua das Flores, Centro, Rio Branco - AC'),
-('1300039359', 'Jeovana Barbosa do Nascimento', '2005-05-28', 'Matriculado', '(68) 99901-0012', 'Rua das Flores, Centro, Rio Branco - AR'),
+('1300039359', 'Jeovana Barbosa do Nascimento', '2005-05-28', 'Matriculado', '(68) 99901-0012', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('1400041155', 'Jhenifer Gurgel de Lima', '2006-04-18', 'Matriculado', '(68) 99901-0013', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('1600041128', 'Kelven Sales de Morais', '2008-04-30', 'Matriculado', '(68) 99901-0014', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('12700041133', 'Luis Felipe Gomes do Nascimento', '2007-04-11', 'Matriculado', '(68) 99901-0015', 'Rua das Flores, Centro, Rio Branco - AC'),
@@ -32,10 +32,10 @@ INSERT INTO alunos (id, nome, data_nascimento, situacao, telefone, endereco) VAL
 ('1900041134', 'Maria Isabel Nascimento Ferreira', '2002-05-26', 'Matriculado', '(68) 99901-0017', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('2000041125', 'Pyetra Menezes', '2009-08-17', 'Matriculado', '(68) 99901-0018', 'Rua das Flores, Centro, Rio Branco - AC'),
 ('2100041137', 'Ronisson Silva do Couto', '2008-10-04', 'Matriculado', '(68) 99901-0019', 'Rua das Flores, Centro, Rio Branco - AC'),
-('2200041148', 'Thallyson Daniel de Souza Pinheiro', '2007-03-19', 'matriculado', '(68) 99901-0020', 'Rua das Flores, Centro, Rio Branco - IC'),
-('22656467', 'KauÃ£ Silva', '2006-04-27', 'Matriculado', '(68) 99901-0021', 'Rua das Flores, Centro, Rio Branco - AC'),
-('345689', 'Luany Lima', '2006-08-09', 'matriculado', '(68) 99901-0022', 'Rua das Flores, Centro, Rio Branco - AC'),
-('2300041150', 'Vitoria Akylla CarlÃ©0 Soares Lima', '2006-02-25', 'Matriculad0', '(68) 99901-0023', 'Rua das Flores, Centro, Rio Branco - PA');
+('2200041148', 'Thallyson Daniel de Souza Pinheiro', '2007-03-19', 'matriculado', '(68) 99901-0020', 'Rua das Flores, Centro, Rio Branco - AC'),
+('22656467', 'Kauã Silva', '2006-04-27', 'Matriculado', '(68) 99901-0021', 'Rua das Flores, Centro, Rio Branco - AC'),
+('345689', 'Luany Lima', '2006-08-09', 'Desmatriculado', '(68) 99901-0022', 'Rua das Flores, Centro, Rio Branco - AC'),
+('2300041150', 'Vitoria Akylla Carlé0 Soares Lima', '2006-02-25', 'Matriculad0', '(68) 99901-0023', 'Rua das Flores, Centro, Rio Branco - AC');
 
 
 
@@ -48,6 +48,16 @@ UPDATE alunos SET id= '800030152' WHERE id='8020030152'
 UPDATE alunos SET id= '1700041133' WHERE id='12700041133'
 UPDATE alunos SET id= '2000041125' WHERE id='22200041125'
 UPDATE alunos SET id= '2100041137' WHERE id='255100041137'
+DELETE FROM Alunos WHERE id = '22656467'
+DELETE FROM Alunos WHERE id = '345689'
+UPDATE Alunos SET situacao  = 'Matriculado'  WHERE id = '2300041150'
+UPDATE Alunos SET situacao  = 'Matriculado'  WHERE id = '2200041148'
+UPDATE Alunos SET nome  = 'Vitoria Akylla Carléo Soares Lima'  WHERE id = '2300041150'
+UPDATE Alunos SET nome  = 'Ana Sara Santos do Nascimento'  WHERE id = '300041117'
+UPDATE Alunos SET nome = 'Fernanda Matos nascimento'  WHERE id = '700040717'
+UPDATE Alunos SET endereco = 'Rua das Flores, Centro, Rio Branco - AC'  WHERE id = '2300041150'
+UPDATE Alunos SET endereco = 'Rua das Flores, Centro, Rio Branco - AC'  WHERE id = '2200041148'
+UPDATE Alunos SET endereco = 'Rua das Flores, Centro, Rio Branco - AC'  WHERE id = '1300039359'
 
 SELECT * FROM Alunos
 
